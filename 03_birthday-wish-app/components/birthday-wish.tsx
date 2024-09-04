be01f0bbd6c0fb589b7caa86b11329de55fabc7f
MehreenMunsifAli <mehreen.munsif.ali@gmail.com>
2024-09-04 07:56:17 -0700

    Add birthday-wish.tsx file

---
+++ 03_birthday-wish-app/components/birthday-wish.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+import {motion, AnimatePresence} from "framer-motion";
+import dynamic from "next/dynamic";
+import { FaBirthdayCake, FaGift } from 'react-icons/fa';
+import { GiBalloons } from 'react-icons/gi';
