c1dc875fdb14647fc1a14c4c611fab84b737fd89
MehreenMunsifAli <mehreen.munsif.ali@gmail.com>
2024-09-28 19:38:29 +0500

    Update bg-color in app/page.tsx

--- 06_digital-clock-app/app/page.tsx
+++ 06_digital-clock-app/app/page.tsx
@@ -2,7 +2,7 @@ import DigitalClockComponent from "@/components/digital-clock";
 
 export default function Home() {
   return (
-    <div className="bg-lime-800">
+    <div className="bg-slate-500">
       <DigitalClockComponent />
     </div>
     
